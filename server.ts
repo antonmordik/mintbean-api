db69373b117878f06b55daf2322f7c03703742ff
Anton Mordik <modriX@users.noreply.github.com>
2020-07-11 01:16:32 +0300

    http server setup

--- server.ts
+++ server.ts
@@ -2,12 +2,19 @@ import {
   WebSocket,
   WebSocketServer,
 } from "https://deno.land/x/websocket/mod.ts";
+import {
+  Application,
+  Router,
+  Status,
+  Context,
+} from "https://deno.land/x/oak/mod.ts";
 import { parseMessage } from "./src/services/parser.ts";
 import Body from "./src/interfaces/Body.ts";
 import Message from "./src/constants/Message.ts";
 import WSError from "./src/constants/WSError.ts";
+import config from "./src/config.ts";
 
-const wss = new WebSocketServer(8080);
+const wss = new WebSocketServer(parseInt(config.WS_PORT));
 
 wss.on("connection", (ws: WebSocket) => {
   ws.on("message", (message: string) => {
@@ -19,3 +26,18 @@ wss.on("connection", (ws: WebSocket) => {
     ws.send(message);
   });
 });
+
+const app = new Application();
+
+const router = new Router();
+
+router.get("/", (ctx: Context) => {
+  ctx.response.status = Status.OK;
+  ctx.response.body = { message: "It's work !" };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log("🚀 Deno server start!");
+await app.listen(`0.0.0.0:${config.API_PORT}`);
